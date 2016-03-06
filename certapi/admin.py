0e782ba20ec60db90947725b51af2cf1e977b672
Bill Blanchard <bill@plumbdev.com>
2016-03-06 11:25:17 -0500

    add admin site

--- certapi/admin.py
+++ certapi/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Badge, Student
+
+
+class BadgeInline(admin.TabularInline):
+    model = Badge
+    fields = ('name', 'is_validated', 'validated_by', 'validated_at', 'completed_date')
+
+
+class StudentAdmin(admin.ModelAdmin):
+    inlines = [BadgeInline]
+
+admin.site.register(Badge)
+admin.site.register(Student, StudentAdmin)
--- validator/settings.py
+++ validator/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'validator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['certapi/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
